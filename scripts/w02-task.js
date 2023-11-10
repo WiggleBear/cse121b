4511f569189dbed791f323c0598dbff7e362083b
WiggleBear <wigglebugc@gmail.com>
2023-11-10 04:36:00 -0700

    test

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -12,39 +12,31 @@ let currentYear = new Date(year);
 
 //Declare and instantiate a variable to hold the file path (location) 
 //and file name of the image that you placed in the images folder as a string.
+
 const profilePicture ='images/photo.png';
 
-/* Step 3 - Element Variables */
+/*Step 3 - Element Variables */
 
 const nameElement = document.getElementById('name');
+
 const foodElement = document.getElementById('food');
+
 const yearElement = document.querySelector('year');
+
 const imageElement = document.querySelector('img');
 
 /* Step 4 - Adding Content */
 
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
+
 yearElement.textContent = currentYear;
+
 imageElement.setAttribute('src', profilePicture);
+
 imageElement.setAttribute('alt', `Profile image of ${fullName}`);
 
 
 /* Step 5 - Array */
-
-const favoriteFoods = ['Pizza', 'Japonese food', 'Rice and Beans', 'Fried chicken']
-
-foodElement.innerHTML += `<br>${favoriteFoods}`;
-
-const newFavoriteFood = 'Ice Cream';
-favoriteFoods.push(newFavoriteFood);
-
-foodElement.innerHTML += `<br>${favoriteFoods}`;
-
-favoriteFoods.shift();
-foodElement.innerHTML += `<br>${favoriteFoods}`;
-
-favoriteFoods.pop();
-foodElement.innerHTML += `<br>${favoriteFoods}`;
 
 
 
